76cb71b9875c8e4e1d970dd722cf695fd3442e31
MatiusDev <mateomonsalve502@gmail.com>
2025-08-06 21:07:56 -0500

    NEW: Init backend

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import { config } from './config/appConfig.js';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(config.PORT, () => {
+    console.log(`Server is running on port ${config.PORT}`);
+})
