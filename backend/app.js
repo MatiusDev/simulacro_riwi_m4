707fd7c6625888d9d0206fe6d78b9fff14da23d8
MatiusDev <mateomonsalve502@gmail.com>
2025-08-07 20:35:59 -0500

    NEW: Adding database dependencies, configuration and connection

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,14 @@ import { config } from './config/appConfig.js';
 import router from './router.js';
 
 const app = express();
+const { SV_PORT, DB_POOL } = config;
 
 app.use('/api', router);
 
-app.listen(config.PORT, () => {
+DB_POOL.connect()
+    .then(() => console.log('Database connected successfully'))
+    .catch(err => console.error('Database connection error:', err));
+
+app.listen(SV_PORT, () => {
     console.log(`Server is running on port ${config.PORT}`);
 })
--- backend/config/appConfig.js
+++ backend/config/appConfig.js
@@ -1,5 +1,22 @@
-const PORT = 3000;
+import { Pool } from 'pg';
+
+const SV_PORT = 3000;
+
+const DB_USER = 'developer';
+const DB_HOST = 'localhost';
+const DB_DATABASE = 'testdb';
+const DB_PASSWORD = '123456';
+const DB_PORT = 5432;
+
+const DB_POOL = new Pool({
+    user: DB_USER,
+    host: DB_HOST,
+    database: DB_DATABASE,
+    password: DB_PASSWORD,
+    port: DB_PORT,
+});
 
 export const config = {
-    PORT,
+    SV_PORT,
+    DB_POOL
 }
